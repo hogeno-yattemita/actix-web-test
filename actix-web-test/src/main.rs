d1b4da5bfc26a35d5060efffa31150cab8ec9879
hogeno <masshi1108@gmail.com>
2022-08-03 12:05:51 +0900

    add api

--- actix-web-test/src/main.rs
+++ actix-web-test/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware::Logger, HttpResponse, Responder, get, HttpServer, App};
+
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/again")]
+async fn again() -> impl Responder{
+    HttpResponse::Ok().body("Hello again!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    log::info!("Starting server...");
+
+    HttpServer::new(||{
+        App::new()
+            .wrap(Logger::default())
+            .service(index)
+            .service(again)
+    })
+    .bind("localhost:8080")
+    .unwrap()
+    .run()
+    .await
 }
