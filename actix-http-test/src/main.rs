b7035019afcb53537c582d218ddd3c2dbc53fbc7
hogeno <masshi1108@gmail.com>
2022-08-17 18:02:54 +0900

    仮作成

--- actix-http-test/src/main.rs
+++ actix-http-test/src/main.rs
@@ -1,6 +1,13 @@
 use std::{thread, time::Duration};
 
-use actix_web::{middleware::Logger, HttpResponse, Responder, get, HttpServer, App};
+use actix_cors::Cors;
+use actix_web::{middleware::Logger, HttpResponse, Responder, get, HttpServer, App, web::Data};
+
+mod db;
+mod handlers;
+mod schemas;
+
+use self::{db::get_db_pool, handlers::register};
 
 
 #[get("/")]
@@ -16,15 +23,24 @@ async fn again() -> impl Responder{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    
+    let pool = get_db_pool();
+    
     log::info!("Starting server...");
 
-    HttpServer::new(||{
+    HttpServer::new(move ||{
         App::new()
+            .app_data(Data::new(pool.clone()))
+            .configure(register)
             .wrap(Logger::default())
             .service(index)
             .service(again)
+            .wrap(Cors::permissive())
+            .wrap(Logger::default())
     })
+    .workers(2)
     .bind("localhost:8080")
     .unwrap()
     .run()
--- docker/mysql/init/1_create.sql
+++ docker/mysql/init/1_create.sql
@@ -1,16 +1,36 @@
 CREATE DATABASE IF NOT EXISTS userdb1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE userdb1;
 
-CREATE TABLE IF NOT EXISTS `todo` (
-  `id` varchar(64) NOT NULL,
-  `text` varchar(256) NOT NULL,
-  `done` bool NOT NULL,
-  `user_id` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
+-- CREATE TABLE IF NOT EXISTS `todo` (
+--   `id` varchar(64) NOT NULL,
+--   `text` varchar(256) NOT NULL,
+--   `done` bool NOT NULL,
+--   `user_id` varchar(64) NOT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+-- CREATE TABLE IF NOT EXISTS `user` (
+--   `id` varchar(64) NOT NULL,
+--   `name` varchar(256) NOT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_fk0` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` varchar(64) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
